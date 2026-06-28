27a0b488f80e78378764caa7bae646ab8c023878
RajGehlot28 <rajgehlot2007@gmail.com>
2026-06-28 18:48:29 +0530

    Update chunk_manager.py

--- backend/chunk_manager.py
+++ backend/chunk_manager.py
@@ -1,16 +1,15 @@
-from langchain_experimental.text_splitters import SemanticChunker
+from langchain_experimental.text_splitter import SemanticChunker
+from sentence_transformer_embeddings import SentenceTransformerEmbeddings
+
 class ChunkManager:
     def __init__(self, documents, embedding_manager):
         self.documents = documents
-        
-        # Initializing real semantic splitter using your local model weights
-        self.text_splitter = SemanticChunker(
-            embedding_manager.model, # passed SentenceTransformer model instance
-            breakpoint_threshold_type="percentile" # Splits when similarity drops based no percentile gap
-        )
-        
-        self.chunks = self.text_splitter.split_documents(self.documents)
-        print("Total semantic chunks generated:", len(self.chunks))
-    
+
+        embeddings = SentenceTransformerEmbeddings(embedding_manager.model)
+        self.text_splitter = SemanticChunker(embeddings, breakpoint_threshold_type="percentile")
+
+        self.chunks = self.text_splitter.split_documents(documents)
+        print("Total semantic chunks:", len(self.chunks))
+
     def get_chunks(self):
         return self.chunks
