58a929311b4222d8aebafbcdb722c89b21b4bc6a
RajGehlot28 <rajgehlot2007@gmail.com>
2026-06-28 18:49:02 +0530

    Update ingest.py

--- backend/ingest.py
+++ backend/ingest.py
@@ -1,30 +1,33 @@
-from pdf_to_doc import PdfToDoc
+from data_to_doc import DataToDoc
 from chunk_manager import ChunkManager
 from embedding_manager import EmbeddingManager
 from vector_store import VectorStore
+import asyncio # to call async function
 
-def store_vectorDB():
+async def store_vectorDB():
     # Ingestion Pipeline :-
 
     print("Starting Ingestion...")
 
     # step-1 converting pdfs to documents
-    pdf_to_doc_manager = PdfToDoc()
-    documents = pdf_to_doc_manager.get_documents()
+    data_to_doc_manager = DataToDoc()
+    documents = data_to_doc_manager.get_documents()
 
     # step-2 splitting documents to chunks
-    chunk_manager = ChunkManager(documents, 1000, 50)
+    embedding_manager = EmbeddingManager()
+    chunk_manager = ChunkManager(documents, embedding_manager)
     chunks = chunk_manager.get_chunks()
 
     # step-3 create embeddings for chunks
-    embedding_manager = EmbeddingManager()
     texts = [chunk.page_content for chunk in chunks]
     embeddings = embedding_manager.generate_embeddings(texts)
 
     # step-4 store embeddings into vectorDB (Qdrant DB)
     vector_store = VectorStore()
-    vector_store.add_documents(chunks, embeddings)
+    await vector_store.add_documents(chunks, embeddings)
 
     print("Ingestion completed")
 
-store_vectorDB()
+
+if __name__ == "__main__":
+    asyncio.run(store_vectorDB())
